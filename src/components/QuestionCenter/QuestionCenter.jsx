1ba1fad154fad3027b856420629a0e55f2866a08
Jason Bastin <bastin.jason@gmail.com>
2021-03-22 22:23:15 -0400

    add game over functionality

--- src/components/QuestionCenter/QuestionCenter.jsx
+++ src/components/QuestionCenter/QuestionCenter.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Options from "../Options/Options";
 import Scoreboard from "../Scoreboard/Scoreboard";
+import GameOver from "../GameOver/GameOver";
 import "./index.css";
 
 const QuestionCenter = ({ currentQuestion, nextQuestion }) => {
@@ -19,28 +20,41 @@ const QuestionCenter = ({ currentQuestion, nextQuestion }) => {
 
   const handleCorrectGuess = () => {};
 
-  return (
-    <div className="question-container">
-      <Scoreboard
-        score={score}
-        question_value={question_value}
-        strikes={strikes}
-      />
-      <div className="question-card">
-        <div className="question">{question}</div>
-      </div>
-      <div className="options-container">
-        <Options
-          options={options}
-          answer={answer}
+  if (strikes.length < 3) {
+    return (
+      <div className="question-container">
+        <Scoreboard
+          score={score}
           question_value={question_value}
-          handleScore={handleScore}
-          handleWrongGuess={handleWrongGuess}
-          nextQuestion={nextQuestion}
+          strikes={strikes}
         />
+        <div className="question-card">
+          <div className="question">{question}</div>
+        </div>
+        <div className="options-container">
+          <Options
+            options={options}
+            answer={answer}
+            question_value={question_value}
+            handleScore={handleScore}
+            handleWrongGuess={handleWrongGuess}
+            nextQuestion={nextQuestion}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <Scoreboard
+          score={score}
+          question_value={question_value}
+          strikes={strikes}
+        />
+        <GameOver />
+      </div>
+    );
+  }
 };
 
 export default QuestionCenter;
