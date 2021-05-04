e25c28ca7a22d4f93508c37b8f2d93bdae2f29fe
Jason Bastin <bastin.jason@gmail.com>
2021-05-04 15:40:47 -0400

    restructured Game Over layout

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -4,12 +4,16 @@ import { Link } from "react-router-dom";
 function GameOver() {
   return (
     <div>
-      <h1>Oh, no! Three strikes and you're out!</h1>
-      <Link to="/GameCenter">
-        <button onClick={() => window.location.reload(false)}>
-          Try Again!
-        </button>
-      </Link>
+      <div className="game-over-title">
+        <h1>Oh, no! Three strikes and you're out!</h1>
+      </div>
+      <div className="try-again">
+        <Link to="/GameCenter">
+          <button onClick={() => window.location.reload(false)}>
+            Try Again!
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
